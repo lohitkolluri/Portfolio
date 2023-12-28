87972046f9cacdf647ad30d1561be0fb74c61570
Lohit Kolluri <lohitkolluri@gmail.com>
2023-12-28 19:41:28 +0530

    Init Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/scss/index.scss";
 import type { AppProps } from "next/app";
 import { Raleway, Fira_Code } from "next/font/google";
 
+
 const raleway = Raleway({ subsets: ["latin"] });
 const firaCode = Fira_Code({
   weight: ["300", "400", "500", "600", "700"],
