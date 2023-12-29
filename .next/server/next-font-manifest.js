fa4d6e0a059fafe72a89531b11a6398be7c77150
Lohit Kolluri <lohitkolluri@gmail.com>
2023-12-29 17:44:31 +0530

    .

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{"/_app":["static/media/beb742c171df2176-s.p.woff2","static/media/122c360d7fe6d395-s.p.woff2"]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":true}';
