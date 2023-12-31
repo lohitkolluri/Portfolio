327ca4850134970e984a663ca510d7d9b8e26c93
Lohit Kolluri <lohitkolluri@gmail.com>
2023-12-31 11:11:09 +0530

    change build folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
   images: {
     domains: ['lohitcdn.blob.core.windows.net'], // Only the root domain
   },  
