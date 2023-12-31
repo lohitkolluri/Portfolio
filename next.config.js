70a57ca24a262a221e59a840d9afe2e85671b317
Lohit Kolluri <lohitkolluri@gmail.com>
2023-12-31 11:18:21 +0530

    💚 Update Workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
+  output: "standalone",
   images: {
     domains: ['lohitcdn.blob.core.windows.net'], // Only the root domain
   },  
