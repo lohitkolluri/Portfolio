8ff19c260f282405f745b07df9a03d1002d883cc
Lohit Kolluri <lohitkolluri@gmail.com>
2024-01-01 15:11:39 +0530

    🩹 Fix Contact

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -19,7 +19,7 @@ function Contact() {
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
         I'm always looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I’ll
+        is always open. Whether you have a question or just want to say hi, I'll
         try my best to get back to you!
       </p>
       <div className="contact-cta">
