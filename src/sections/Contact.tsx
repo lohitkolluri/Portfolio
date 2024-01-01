5a973b5f5d362ab396240fd52d914a49953ee574
Lohit Kolluri <lohitkolluri@gmail.com>
2024-01-01 15:10:07 +0530

    Minor TouchUP

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -18,7 +18,7 @@ function Contact() {
       <h2 className="contact-title">What&apos;s Next?</h2>
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
-        Although I’m not currently looking for any new opportunities, my inbox
+        I'm always looking for any new opportunities, my inbox
         is always open. Whether you have a question or just want to say hi, I’ll
         try my best to get back to you!
       </p>
