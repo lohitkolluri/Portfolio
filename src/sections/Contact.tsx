6ba8c5254b879df0478411d411bb40d66fa788bc
Lohit Kolluri <lohitkolluri@gmail.com>
2024-01-19 14:01:37 +0530

    :recycle: Refactor Code

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -19,11 +19,11 @@ const Contact = () => {
       transition={{ duration: 0.6 }}
       viewport={{ once: true }}
     >
-      <h2 className="contact-title">What's Next?</h2>
+      <h2 className="contact-title">What&apos;s Next?</h2>
       <h2 className="contact-sub-title">Get In Touch</h2>
       <p className="contact-text">
-        I'm always looking for new opportunities, and my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to
+        I&apos;m always looking for new opportunities, and my inbox is always open.
+        Whether you have a question or just want to say hi, I&apos;ll try my best to
         get back to you!
       </p>
       <div className="contact-cta">
